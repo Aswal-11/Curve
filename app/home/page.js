337410575573ed3768711282c5bb887867348b4f
Aswal-11 <kartikaswal2001@gmail.com>
2025-11-28 08:14:43 +0530

    desiign

--- app/home/page.js
+++ app/home/page.js
@@ -10,12 +10,11 @@ export default function Page() {
         </p>
       </div>
 
-
-      <div className='flex justify-center font-bold'>
-        <div className='flex justify-center mx-nine mx-ten pr-1 lg:w-4/6 max-sm:gap-1'>
-          <p className='w-1/2 lg:w-2/5 max-sm:text-right md:text-center  text-lg sm:text-xl md:text-5xl lg:text-4xl xl:text-6xl'>Tomorrow's</p>
-          <div className='w-1/2 lg:w-3/5'>
-            <div className="relative h-7 md:h-14 xl:h-18 flex w-full items-center justify-center overflow-hidden border-[0.5px] rounded-full text-[18px] sm:text-2xl md:text-[52px] lg:text-[40px] xl:text-[67px] inset-0 ">
+      <div className='flex justify-center items-center font-bold w-full'>
+        <div className='flex justify-center items-center mx-nine mx-ten lg:w-4/6 xl:w-3/5 gap-3'>
+          <p className='w-2/5 lg:w-2/5 xl:w-2/6 max-sm:text-right md:text-center  text-lg sm:text-xl md:text-5xl lg:text-4xl xl:text-6xl'>Tomorrow's</p>
+          <div className='w-4/5 lg:w-3/5 xl:w-4/6'>
+            <div className="relative h-7 md:h-14 xl:h-18 flex w-full items-center justify-center overflow-hidden border-[0.5px] rounded-full text-[19px] sm:text-2xl md:text-[52px] lg:text-[40px] xl:text-[67px] inset-0 ">
               <div className="absolute inset-0 flex items-center justify-center">
                 <h1 className="roll-text roll-text-1 font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-600 opacity-0">
                   INNOVATION
